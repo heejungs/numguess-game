62f54aa2f793adfd530c93ee2232009d26540521
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-01-30 15:08:12 +0900

    feat: Declare the essential variable.
    - Use random library to generate answer number.
    - Print prompt message to get username from user.
    - Get username from user.
    - Print prompt message to get guess number from user.
    - Get guess number from user.(Integer value)

--- numguess.py
+++ numguess.py
@@ -11,3 +11,12 @@ answer = random.randint(1,100)
 username = input("Hi there, What's your name?? ")
 # Print prompt message and get the guess number from user. 
 guess = int(input("Hi, "+ username + "guess the number: "))
+
+# Conditional statement
+# Check the guess number whether it is equal to answer or not.
+if guess == answer:
+    # To concatenate the answer value with prompt message, casting the answer value as string value.
+	print("Correct! The answer was ", str(answer))
+else:
+    # To concatenate the answer value with prompt message, casting the answer value as string value.
+	print("That's not what I wanted!! The answer was ", str(answer))
